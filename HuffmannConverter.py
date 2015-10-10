ebfb194ceef3a5bc42c9351d53b38f06fcc7086c
eroncevich <eroncevich@gmail.com>
2015-10-10 13:35:45 -0600

    Input File, converts to first letters

--- HuffmannConverter.py
+++ HuffmannConverter.py
@@ -1,4 +1,9 @@
+#from OpenSSL import SSL
 import FreqNode
+import sys
+from Crypto.Cipher import AES
+import binascii
+
 letterCode={'e':00000
 ,'x':000010000
 ,'z':000010001
@@ -26,29 +31,59 @@ letterCode={'e':00000
 ,'s':1101
 ,'t':111
 }
+if len(sys.argv)<2:
+    print "Error input missing"
+    print "python HuffmanConverter.py [input]"
+    exit()
+pt = file(sys.argv[1]).read()
+print "Plaintext", pt
+print "Pt Hex", pt.encode('hex')
+ 
+key = 'ihavespecialeyes'
+pt+= "1"
+while(len(pt)%16 !=0):
+    pt+="0"
+print pt
+ 
+encobj = AES.new(key, AES.MODE_ECB)
+ct = encobj.encrypt(pt)
+#print "len", len(ct)
+#print ct
 
-#sampleNum = 923903290329803428904
-sampleNum = 0x9329048904308932489fde
+print "CipherText",ct.encode('hex')
+ctLen = len(ct)*8;
+#print ctLen
+
+binStr = '{0:08b}'.format(int(ct.encode('hex'),16))
+binStr= "0"*(ctLen-len(binStr))+binStr
+#if len(binStr%16)
+#print binary_string, len(binary_string)
+
+
+
+#binStr=''.join(format(ord(x), 'b') for x in ct)
+#print "len", len(binStr)
+#print binStr
 
 hTree = FreqNode.genEncoding()
 ##sampleBinary = "".join(format(ord(x), 'b') for x in sampleNum)
-binStr ='{0:08b}'.format(sampleNum)
+#binStr ='{0:08b}'.format(sampleNum)
 def getChar(binaryStr, hTree):
     if len(binaryStr)==0:
         return ('!',"")
     if(binaryStr[0]=='0'):
-        print "left"
+        #print "left"
         nextTree = hTree.left
         if(nextTree.letter!='!'):
             return (nextTree.letter, binaryStr)
         return getChar(binaryStr[1::], nextTree)
     if(binaryStr[0]=='1'):
-        print "right"
+        #print "right"
         nextTree = hTree.right
         if(nextTree.letter!='!'):
             return (nextTree.letter, binaryStr)
         return getChar(binaryStr[1::], nextTree)
-print binStr
+print binStr, len(binStr)
 
 outStr = ""
 while 1:
@@ -57,5 +92,3 @@ while 1:
     if(c=='!'):
         break
 print outStr
-
-#for i in range(0,len(sampleBinary)):
